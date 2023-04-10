9136b5ea654b6efc784d442663d8522632cfcd95
Vivaldi Rinaldi <varinaldigx@gmail.com>
2023-04-10 18:04:48 -0400

    tested out 18

--- model_dropout.py
+++ model_dropout.py
@@ -65,7 +65,7 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        # out = self.layer4(out)
+        out = self.layer4(out)
         out = self.avg_pool(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
