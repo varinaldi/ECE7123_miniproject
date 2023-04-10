1dbb9d7f6d6d6688b05e13a18ed579c0d64f03a0
Vivaldi Rinaldi <varinaldigx@gmail.com>
2023-04-10 12:42:17 -0400

    updated

--- model.py
+++ model.py
@@ -40,14 +40,12 @@ class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
         super(ResNet, self).__init__()
         self.in_channels = 16
-
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         
         self.layer1 = self._make_layer(block, 16, layers[0], stride=1)
         self.layer2 = self._make_layer(block, 32, layers[1], stride=1)
         self.layer3 = self._make_layer(block, 64, layers[2], stride=1)
-        # self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(64*block.expansion, num_classes)
 
@@ -64,7 +62,6 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        # out = self.layer4(out)
         out = self.avg_pool(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
