1de7a45048c7db040ccbf5c016eeb957bc0acb5a
Vivaldi Rinaldi <varinaldigx@gmail.com>
2023-04-10 23:16:33 -0400

    add learning scheduler

---
+++ dataset.py
@@ -0,0 +1,56 @@
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.utils.data as data
+import copy
+import random
+
+ROOT = '.data'
+train_data = datasets.CIFAR10(root = ROOT, 
+                              train = True, 
+                              download = True)
+                              
+means = train_data.data.mean(axis = (0,1,2)) / 255
+stds = train_data.data.std(axis = (0,1,2)) / 255
+
+
+# ----- augment data -----
+augment = transforms.Compose([
+                        #    transforms.RandomRotation(5),
+                           transforms.RandomHorizontalFlip(0.5),
+                           transforms.RandomCrop(32, padding = 2),
+                           transforms.ToTensor(),
+                           transforms.Normalize(mean = means, 
+                                                std = stds)
+                       ])
+
+augment_test = transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize(mean = means, 
+                                                std = stds)
+                       ])
+
+
+# ---- Load and transform CIFAR10 -----
+ROOT = '.data'
+train_data = datasets.CIFAR10(root = ROOT, 
+                              train = True, 
+                              download = True, 
+                              transform = augment)
+
+test_data = datasets.CIFAR10(root = ROOT, 
+                             train = False, 
+                             download = True, 
+                             transform= augment_test)
+
+
+# ---- create validadation -----
+VALID_RATIO = 0.9
+
+n_train_examples = int(len(train_data) * VALID_RATIO)
+n_valid_examples = len(train_data) - n_train_examples
+
+train_data, valid_data = data.random_split(train_data, 
+                                           [n_train_examples, n_valid_examples])
+
+valid_data = copy.deepcopy(valid_data)
+valid_data.dataset.transform = augment_test
