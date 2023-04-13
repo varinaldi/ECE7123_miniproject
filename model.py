eef380bb652fe3f6b0fa46175001cb0a4fcb8eea
Vivaldi Rinaldi <varinaldigx@gmail.com>
2023-04-13 02:27:52 -0400

    modified finalize

--- model.py
+++ model.py
@@ -12,14 +12,13 @@ import torch.utils.data as data
 class ResidualBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_channels, out_channels, stride=1, dropout_rate=0.5):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         
-        self.dropout = nn.Dropout(p=dropout_rate)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != self.expansion*out_channels:
@@ -30,35 +29,37 @@ class ResidualBlock(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = self.dropout(out)
         out = self.bn2(self.conv2(out))
-        out = self.dropout(out)
         out += self.shortcut(x)
         out = F.relu(out)
         return out
     
 
 
+# --------------------------------
+# ----- ResNet Architecture ------
+# --------------------------------
+
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10, dropout_rate=0.5):
+    def __init__(self, block, layers, num_classes=10):
         super(ResNet, self).__init__()
         self.in_channels = 16
 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         
-        self.layer1 = self._make_layer(block, 16, layers[0], stride=1, dropout_rate=dropout_rate)
-        self.layer2 = self._make_layer(block, 32, layers[1], stride=2, dropout_rate=dropout_rate)
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2, dropout_rate=dropout_rate)
+        self.layer1 = self._make_layer(block, 16, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(64*block.expansion, num_classes)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride, dropout_rate ):
+    def _make_layer(self, block, out_channels, num_blocks, stride ):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride, dropout_rate=dropout_rate))
+            layers.append(block(self.in_channels, out_channels, stride))
             self.in_channels = out_channels * block.expansion
         return nn.Sequential(*layers)
 
--- train_eval_util.py
+++ train_eval_util.py
@@ -72,8 +72,48 @@ def evaluate(model, iterator, criterion, device):
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 def total_layers(model):
-    return sum([1 for m in model.modules() if isinstance(m, nn.Conv2d)]) - 2
+    return  sum([1 for _ in model.modules() if isinstance(_, nn.Conv2d) or isinstance(_, nn.Linear)]) - 2 #
 
 def total_params(model):
      return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def get_predictions(model, iterator, device):
+
+    model.eval()
+
+    labels = []
+    probs = []
+
+    # Q4: Fill code here.
+    for (x, y) in iterator:
+
+            x = x.to(device)
+            y = y.to(device)
+
+            labels.append(y)
+            ypred  = model(x)
+            p = F.softmax(ypred, dim =1)
+            probs.append(p)
+            
+   
+    labels = torch.cat(labels, dim = 0)
+    probs = torch.cat(probs, dim = 0)
+
+    return labels, probs
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+def plot_confusion_matrix(labels, pred_labels, classes):
+    
+    fig = plt.figure(figsize = (10, 10));
+    ax = fig.add_subplot(1, 1, 1);
+    cm = confusion_matrix(labels, pred_labels);
+    cm = ConfusionMatrixDisplay(cm, display_labels = classes);
+    cm.plot(values_format = 'd', cmap = 'Blues', ax = ax)
+    plt.xticks(rotation = 20)
